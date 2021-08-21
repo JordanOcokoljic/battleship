946eb70fe49359d2b06629af971ed13def7cdeeb
Jordan Ocokoljic <jordan.ocokoljic@5c6e.com>
2021-08-21 10:36:41 +1000

    Implement Player to be able to play the game.

---
+++ State.Test/PlayerTest.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace State.Test
+{
+    [TestFixture]
+    public class PlayerTest
+    {
+        [Test]
+        public void NameReturnsTheNameAssociatedWithThePlayer()
+        {
+            var player = new Player("Jordan", new Board());
+            Assert.That(player.Name, Is.EqualTo("Jordan"));
+        }
+        
+        [Test]
+        public void BoardReturnsTheBoardAssociatedWithThePlayer()
+        {
+            var board = new Board();
+            var player = new Player("Jordan", board);
+            Assert.That(player.Board, Is.SameAs(board));
+        }
+    }
+}
---
+++ State/Player.cs
@@ -0,0 +1,33 @@
+namespace State
+{
+    /// <summary>
+    ///     Class <c>Player</c> represents a player in a game of Battleship.
+    /// </summary>
+    public class Player
+    {
+        /// <summary>
+        ///     The in game name of the player.
+        /// </summary>
+        public readonly string Name;
+        
+        /// <summary>
+        ///     The <see cref="Board"/> associated with this <see cref="Player"/>.
+        /// </summary>
+        public readonly Board Board;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Player"/> class.
+        /// </summary>
+        /// <param name="name">
+        ///     The in game name the player has chosen to use.
+        /// </param>
+        /// <param name="board">
+        ///     The <see cref="Board"/> the player controls.
+        /// </param>
+        public Player(string name, Board board)
+        {
+            Name = name;
+            Board = board;
+        }
+    }
+}
